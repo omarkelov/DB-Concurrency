5dc93d85d9cdfb678392ad4c3e577775f90765bb
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-09-23 18:33:50 +0700

    Task_2 InterruptedException ignored

--- Concurrency_2/src/main/java/ru/nsu/fit/markelov/Main.java
+++ Concurrency_2/src/main/java/ru/nsu/fit/markelov/Main.java
@@ -9,10 +9,7 @@ public class Main {
 
             childThread.start();
             childThread.join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println(Thread.currentThread().getName() + " thread is interrupted.");
-        }
+        } catch (InterruptedException ignored) {}
 
         printLines();
     }
