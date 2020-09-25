759545ac250962d1879d0b7d3048638bbdfc3b17
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-09-25 13:27:19 +0700

    Task_7 threadsCount is gotten from args

--- Concurrency_7/src/main/java/ru/nsu/fit/markelov/Main.java
+++ Concurrency_7/src/main/java/ru/nsu/fit/markelov/Main.java
@@ -2,6 +2,18 @@ package ru.nsu.fit.markelov;
 
 public class Main {
     public static void main(String[] args) {
-        new Founder(new Company(1, 8)).start();
+        int threadsCount = Runtime.getRuntime().availableProcessors();
+
+        try {
+            threadsCount = Integer.parseInt(args[0]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Threads count has not been provided through arguments. " +
+                threadsCount + " threads will be used.");
+        } catch (NumberFormatException e) {
+            System.out.println("Threads count could not be parsed as an integer. " +
+                threadsCount + " threads will be used.");
+        }
+
+        new Founder(new Company(threadsCount, 8)).start();
     }
 }
